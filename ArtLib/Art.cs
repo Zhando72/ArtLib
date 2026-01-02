feacf16d01abdcc6fba9a018dcfcf93cf94467db
Zhando72 <lasse.kellberg@gmail.com>
2026-01-02 14:38:36 +0100

    Add project files.

---
+++ ArtLib/Art.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtLib
+{
+    public class Art
+    {
+        #region Properties
+        private int Id, Price;
+        private string ArtName, ArtType;
+        #endregion
+
+        public int id { get; }
+        public int price { get; }
+        public string artName { get; }
+        public string artType { get; }
+
+        public Art(int id, int price, string artName, string artType)
+        {
+            Id = id;
+            Price = price;
+            ArtName = artName;
+            ArtType = artType;
+        }
+
+        public override string ToString()
+        {
+            return $"The art {ArtName} has an ID of {Id}. It is the type {ArtType}, and will cost {Price}";
+        }
+    }
+}
---
+++ ArtLib/ArtsRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtLib
+{
+    public class ArtsRepository
+    {
+        private int NextId = 0;
+        List<Art> artsList = new List<Art>();
+
+
+        public List<Art> GetAll()
+        {
+            return artsList;
+        }
+
+        public Art GetById(int Id)
+        {
+            foreach (Art A in artsList)
+            {
+                if (Id == A.id)
+                {
+                    return A;
+                }
+            }
+            return null;
+        }
+
+        public void add(int price, string artName, string artType)
+        {
+            Art art = new Art(NextId, price, artName, artType);
+            artsList.Add(art);
+            NextId++;
+        }
+
+        public void delete(int Id)
+        {
+            foreach (Art A in artsList)
+            {
+                if (Id == A.id)
+                {
+                    artsList.Remove(A);
+                }
+            }
+        }
+    }
+}
